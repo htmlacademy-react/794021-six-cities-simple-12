0aee2ecc12b5840afa43eab2c40705963df28955
rrramble <rustam.arkharov@gmail.com>
2023-03-04 01:22:25 +0600

    chore: remove irrelevant test

--- project/src/components/app/app.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import App from './app';
-
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/312 places to stay in Amsterdam/i);
-  expect(textElement).toBeInTheDocument();
-});
