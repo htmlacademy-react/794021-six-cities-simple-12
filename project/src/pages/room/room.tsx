a063252ade38f37a9b4149a285566c69ae48e86f
rrramble <rustam.arkharov@gmail.com>
2023-03-05 03:05:02 +0600

    feature: use 'goods: HardwareFeatures' as props

--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -72,36 +72,15 @@ function Room({ offer }: RoomType): JSX.Element {
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
                 <ul className="property__inside-list">
-                  <li className="property__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="property__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="property__inside-item">
-                    Towels
-                  </li>
-                  <li className="property__inside-item">
-                    Heating
-                  </li>
-                  <li className="property__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="property__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="property__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="property__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="property__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="property__inside-item">
-                    Fridge
-                  </li>
+                  {
+                    offer.goods.map((good) => (
+                      <li className="property__inside-item"
+                        key={good}
+                      >
+                        {good}
+                      </li>
+                    ))
+                  }
                 </ul>
               </div>
               <div className="property__host">
