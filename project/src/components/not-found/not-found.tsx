eeafdc890b6c4f7197e5dc4a1f913b9d3b421d13
rrramble <rustam.arkharov@gmail.com>
2023-03-05 23:14:24 +0600

    feat: add inline styles

--- project/src/components/not-found/not-found.tsx
+++ project/src/components/not-found/not-found.tsx
@@ -1,16 +1,30 @@
+import { CSSProperties } from 'react';
 import { Link } from 'react-router-dom';
 
 export default NotFound;
 
+const blockStyle: CSSProperties = {
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+const linkStyle: CSSProperties = {
+  textDecoration: 'revert',
+};
+
 function NotFound(): JSX.Element {
   return (
-    <>
+    <div
+      style={blockStyle}
+    >
       <h1>404 Not Found</h1>
       <Link
+        style={linkStyle}
         to='/'
       >
         Go to the main page
       </Link>
-    </>
+    </div>
   );
 }
