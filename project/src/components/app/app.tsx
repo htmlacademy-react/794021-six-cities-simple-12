e9699bfb49fa5c0e9ccab25f538d6bda017e0071
rrramble <rustam.arkharov@gmail.com>
2023-03-05 23:54:27 +0600

    feature: add Routes to all app pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from 'src/pages/main/main';
+import Login from 'src/pages/login/login';
+import Room from 'src/pages/room/room';
+import NotFound from 'src/components/not-found/not-found';
 import { Cities, City, Offers, UserLogin } from 'src/types/types';
 
 type AppProps = {
@@ -10,10 +14,23 @@ type AppProps = {
 };
 
 function App(props: AppProps): JSX.Element {
+  const [ firstOffer ] = props.offers;
   return (
-    <Main
-      {...props}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Main {...props} />}/>
+          <Route path='login' element={<Login />} />
+          <Route
+            path='offer/:id'
+            element={
+              <Room offer={firstOffer} userLogin={props.userLogin} />
+            }
+          />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
