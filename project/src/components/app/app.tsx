8612c6b536879b1573204c493c730144cdbe9574
rrramble <rustam.arkharov@gmail.com>
2023-03-05 14:07:11 +0600

    feat: remove displaying Login and Room pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,4 @@
-import Login from 'src/pages/login/login';
 import Main from 'src/pages/main/main';
-import Room from 'src/pages/room/room';
 import { Cities, City, Offers, UserLogin } from 'src/types/types';
 
 type AppProps = {
@@ -12,18 +10,10 @@ type AppProps = {
 };
 
 function App(props: AppProps): JSX.Element {
-  const { offers: [ offer ] } = props;
-  const { userLogin } = props;
-
-  // FIXME: display all components at once as a proof-of-concept
   return (
-    <>
-      <Login />
-      <Room offer={offer} userLogin={userLogin} />
-      <Main
-        {...props}
-      />
-    </>
+    <Main
+      {...props}
+    />
   );
 }
 
