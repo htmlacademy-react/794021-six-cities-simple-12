e9e6b20872ac295262186fe71e8ad7262e8bb581
rrramble <rustam.arkharov@gmail.com>
2023-03-06 13:22:59 +0600

    refactor: extract and use 'RoomHost' component

--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -2,6 +2,7 @@ import HeaderRoom from 'src/components/header-room/header-room';
 import NearPlacesCards from 'src/components/near-places-cards/near-places-cards';
 import RoomGallery from 'src/components/room-gallery/room-gallery';
 import RoomHardwareFeatures from 'src/components/room-hardware-features/room-hardware-features';
+import RoomHost from 'src/components/room-host/room-host';
 import RoomReviews from 'src/components/room-reviews/room-reviews';
 import { Offer, UserLogin } from 'src/types/types';
 import { getPercentFromRating, capitalizeFirstLetter } from 'src/utils/utils';
@@ -23,14 +24,12 @@ function Room({ offer, userLogin }: RoomProps): JSX.Element {
 
           <div className="property__container container">
             <div className="property__wrapper">
-
               {
                 offer.isPremium &&
                   <div className="property__mark">
                     <span>Premium</span>
                   </div>
               }
-
               <div className="property__name-wrapper">
                 <h1 className="property__name">
                   {offer.title}
@@ -59,29 +58,7 @@ function Room({ offer, userLogin }: RoomProps): JSX.Element {
                 <span className="property__price-text">&nbsp;night</span>
               </div>
               <RoomHardwareFeatures goods={offer.goods}/>
-              <div className="property__host">
-                <h2 className="property__host-title">Meet the host</h2>
-                <div className="property__host-user user">
-                  <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
-                  </div>
-                  <span className="property__user-name">
-                    Angelina
-                  </span>
-                  <span className="property__user-status">
-                    Pro
-                  </span>
-                </div>
-                <div className="property__description">
-                  <p className="property__text">
-                    A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                  </p>
-                  <p className="property__text">
-                    An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
-                  </p>
-                </div>
-              </div>
-
+              <RoomHost />
               <section className="property__reviews reviews">
                 <RoomReviews />
               </section>
