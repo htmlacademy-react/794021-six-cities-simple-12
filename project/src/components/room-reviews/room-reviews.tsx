79efbe111fb7792939160204a1cfa0d44ba7030f
rrramble <rustam.arkharov@gmail.com>
2023-03-07 18:46:13 +0600

    fix: add JS {' '} space to multiline string

--- project/src/components/room-reviews/room-reviews.tsx
+++ project/src/components/room-reviews/room-reviews.tsx
@@ -54,7 +54,7 @@ function RoomReviews({ reviews }: RoomReviewProps): JSX.Element {
             To submit review please make sure to set
             {' '}<span className="reviews__star">rating</span>
             {' '}and describe your stay with at least
-            <b className="reviews__text-amount">50 characters</b>.
+            {' '}<b className="reviews__text-amount">50 characters</b>.
           </p>
           <button className="reviews__submit form__submit button" type="submit" disabled>
             Submit
