1a7f9416798e952cad8b080b46aae793ccdc18d6
rrramble <rustam.arkharov@gmail.com>
2023-03-08 10:17:57 +0600

    refactor: extract and import 'NavHeader' component

--- project/src/components/header-main/header-main.tsx
+++ project/src/components/header-main/header-main.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { UserLogin } from 'src/types/types';
-import LoginLogout from './login-logout';
+import HeaderNav from './header-nav';
+import { useLocation } from 'react-router-dom';
+import { AppRoute } from 'src/utils/consts';
 
 type HeaderMainProps = {
   userLogin: UserLogin;
@@ -9,32 +11,22 @@ type HeaderMainProps = {
 export default HeaderMain;
 
 function HeaderMain({ userLogin }: HeaderMainProps): JSX.Element {
+  const { pathname } = useLocation();
+  const linkClassName = pathname === AppRoute.Root ?
+    'header__logo-link header__logo-link--active' :
+    'header__logo-link';
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link header__logo-link--active" to="/">
+            <Link className={linkClassName} to="/">
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
             </Link>
           </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              {
-                userLogin &&
-                <li className="header__nav-item user">
-                  <div className="header__nav-profile">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">{userLogin}</span>
-                  </div>
-                </li>
-              }
 
-              <li className="header__nav-item">
-                <LoginLogout userLogin={userLogin} />
-              </li>
-            </ul>
-          </nav>
+          {pathname !== AppRoute.Login ? <HeaderNav userLogin={userLogin} /> : null}
         </div>
       </div>
     </header>
