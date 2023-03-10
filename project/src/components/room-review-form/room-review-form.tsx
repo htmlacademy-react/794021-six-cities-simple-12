c497025c80f8cc991e763a5edfbca43748a7aa30
rrramble <rustam.arkharov@gmail.com>
2023-03-11 05:55:01 +0600

    refactor: make state fields all string

--- project/src/components/room-review-form/room-review-form.tsx
+++ project/src/components/room-review-form/room-review-form.tsx
@@ -2,22 +2,18 @@ import OneStarInput from './one-star-input';
 import { RoomReview } from 'src/utils/consts';
 import { ChangeEvent, useEffect, useState, FormEvent } from 'react';
 
-type InputElement = HTMLTextAreaElement | HTMLInputElement;
+type InputElement = HTMLTextAreaElement | HTMLInputElement
+type RoomReviewFormData = { rating: string; review: string }
 // TODO: reset form on every change of Room
 function RoomReviewForm() {
   const [ formData, setFormData ] = useState({
-    rating: 0,
+    rating: '0',
     review: '',
-  });
+  } as RoomReviewFormData);
   const [ isSubmitDisabled, setIsSubmitDisabled ] = useState(true);
 
-  const changeHandler = ({ target }: ChangeEvent<InputElement>): void => {
-    const { name, value } = target;
-    if (name === 'rating') {
-      const rating = parseInt(value, 10);
-      setFormData({ ...formData, [name]: rating });
-      return;
-    }
+  const handleChange = (evt: ChangeEvent<InputElement>): void => {
+    const { name, value } = evt.target;
     setFormData({ ...formData, [name]: value });
   };
 
@@ -26,14 +22,10 @@ function RoomReviewForm() {
   };
 
   useEffect(() => {
-    if (
-      formData.review.length >= RoomReview.TextCharacterMinLimit &&
-      formData.rating > 0
-    ) {
-      setIsSubmitDisabled(false);
-    } else {
-      setIsSubmitDisabled(true);
-    }
+    const isFulfilled = isFormComplete(
+      formData, RoomReview.TextCharacterMinLimit
+    );
+    setIsSubmitDisabled(!isFulfilled);
   }, [formData]);
 
   return (
@@ -47,25 +39,27 @@ function RoomReviewForm() {
         {RoomReview.HeaderText}
       </label>
       <div className="reviews__rating-form form__rating">
-        <OneStarInput htmlId='5-stars' labelTitle='perfect' value='5' onChange={changeHandler} />
-        <OneStarInput htmlId='4-stars' labelTitle='good' value='4' onChange={changeHandler} />
-        <OneStarInput htmlId='3-stars' labelTitle='not bad' value='3' onChange={changeHandler} />
-        <OneStarInput htmlId='2-stars' labelTitle='badly' value='2' onChange={changeHandler} />
-        <OneStarInput htmlId='1-star' labelTitle='terribly' value='1' onChange={changeHandler} />
+        <OneStarInput htmlId='5-stars' labelTitle='perfect' value='5' onChange={handleChange} />
+        <OneStarInput htmlId='4-stars' labelTitle='good' value='4' onChange={handleChange} />
+        <OneStarInput htmlId='3-stars' labelTitle='not bad' value='3' onChange={handleChange} />
+        <OneStarInput htmlId='2-stars' labelTitle='badly' value='2' onChange={handleChange} />
+        <OneStarInput htmlId='1-star' labelTitle='terribly' value='1' onChange={handleChange} />
       </div>
       <textarea className="reviews__textarea form__textarea"
         id="review" name="review"
-        onChange={changeHandler}
+        onChange={handleChange}
         placeholder={RoomReview.PlaceholderText}
         value={formData.review}
       >
       </textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please make sure to set
-          {' '}<span className="reviews__star">rating</span>
-          {' '}and describe your stay with at least
-          {' '}<b className="reviews__text-amount">{RoomReview.TextCharacterMinLimit} characters</b>.
+          To submit review please make sure to set{' '}
+          <span className="reviews__star">rating</span>{' '}
+          and describe your stay with at least{' '}
+          <b className="reviews__text-amount">
+            {RoomReview.TextCharacterMinLimit} characters
+          </b>.
         </p>
         <button
           className="reviews__submit form__submit button"
@@ -77,6 +71,14 @@ function RoomReviewForm() {
       </div>
     </form>
   );
+}
+
+function isFormComplete(
+  formData: RoomReviewFormData,
+  textCharacterMinLimit: number
+): boolean {
+  return formData.review.length < textCharacterMinLimit ||
+    parseInt(formData.rating, 10) < 1;
 }
 
 export default RoomReviewForm;
