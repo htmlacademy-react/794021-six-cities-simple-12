8b14b912197cccd5ad6dbdff4cce42e779ec66c2
rrramble <rustam.arkharov@gmail.com>
2023-03-26 00:30:12 +0600

    feat: add action creator 'changeCity'

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,3 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
+import { CityName } from 'src/types/types';
 
 export const resetCity = createAction('city/reset');
+
+export const changeCity = createAction<CityName>('city/change');
