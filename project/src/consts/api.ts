ac77c1177d246a4fb047e2e903f47dccde435cd4
rrramble <rustam.arkharov@gmail.com>
2023-04-03 19:45:41 +0600

    feat: add API route for 'Offers' fetching

--- project/src/consts/api.ts
+++ project/src/consts/api.ts
@@ -1,6 +1,10 @@
 export const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
 export const BACKEND_URL_DESCRIPTION = 'https://12.react.pages.academy/six-cities-simple/spec';
 
+export enum APIRoute {
+  Offers = '/hotels',
+}
+
 export const TOKEN_HEADER_NAME = 'X-Token';
 
 export const REQUEST_TIMEOUT = 5000;
