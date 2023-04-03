22f6e0f5d7b1a79491802e7d25b2e83f4e62588f
rrramble <rustam.arkharov@gmail.com>
2023-04-03 16:45:51 +0600

    feat: add consts for API

---
+++ project/src/consts/api.ts
@@ -0,0 +1,6 @@
+export const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
+export const BACKEND_URL_DESCRIPTION = 'https://12.react.pages.academy/six-cities-simple/spec';
+
+export const TOKEN_HEADER_NAME = 'X-Token';
+
+export const REQUEST_TIMEOUT = 5000;
