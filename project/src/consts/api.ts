6c5107e577dfd45eef54b699cc9d2ea598734625
rrramble <rustam.arkharov@gmail.com>
2023-04-03 17:09:53 +0600

    feat: add enum for Authorization status

--- project/src/consts/api.ts
+++ project/src/consts/api.ts
@@ -4,3 +4,9 @@ export const BACKEND_URL_DESCRIPTION = 'https://12.react.pages.academy/six-citie
 export const TOKEN_HEADER_NAME = 'X-Token';
 
 export const REQUEST_TIMEOUT = 5000;
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
