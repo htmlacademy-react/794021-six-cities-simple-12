e47ee3a03b96a1d131099c6ca3d936d944af99c2
rrramble <rustam.arkharov@gmail.com>
2023-04-05 00:01:42 +0600

    refactor: rename func to 'setToken()' from 'saveToken()'

--- project/src/services/token.ts
+++ project/src/services/token.ts
@@ -7,7 +7,7 @@ export const getToken = (): Token => {
   return token ?? '';
 };
 
-export const saveToken = (token: Token): void => {
+export const setToken = (token: Token): void => {
   localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
 };
 
