b446d1ac9915f1994c495f9dda1a04d6b0d8a976
rrramble <rustam.arkharov@gmail.com>
2023-04-05 13:55:40 +0600

    refactor: combine <Header> with <HeaderNav>

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,25 +1,42 @@
 import { MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
-import HeaderNav from './header-nav';
 import { useLocation } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from 'src/hooks';
+import { useAppDispatch } from 'src/hooks';
 import { isCurrentPage } from 'src/utils/utils';
 import { AppRoute } from 'src/consts/consts';
-import { AuthorizationStatus } from 'src/consts/api';
 import { logUserOut } from 'src/store/api-actions';
 
-function Header(): JSX.Element {
+type HeaderProps = {
+  isAuthorized: boolean;
+  isNotAuthorized: boolean;
+  userLogin: string;
+}
+
+function Header(props: HeaderProps): JSX.Element {
   const { pathname: currentPath } = useLocation();
-  // TODO: coponents must not ask for state! Only Pages may. Move to Page or App
-  const userLogin = useAppSelector((state) => state.userLogin);
-  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
   const dispatch = useAppDispatch();
+
+  const href = props.isNotAuthorized ? '' : AppRoute.Login;
+  let signInOutClassName: string;
+  let linkText: string;
+
+  if (props.isAuthorized) {
+    signInOutClassName = 'header__signout';
+    linkText = 'Sign out';
+  } else if (props.isNotAuthorized) {
+    signInOutClassName = 'header__login';
+    linkText = 'Sign in';
+  } else {
+    signInOutClassName = '';
+    linkText = '';
+  }
+
   const linkClassName = isCurrentPage(currentPath, AppRoute.Root) ?
     'header__logo-link header__logo-link--active' :
     'header__logo-link';
 
   const handleOnClick = (evt: MouseEvent) => {
-    if (authorizationStatus !== AuthorizationStatus.Authorized) {
+    if (props.isAuthorized) {
       return;
     }
 
@@ -40,10 +57,34 @@ function Header(): JSX.Element {
           {
             isCurrentPage(currentPath, AppRoute.Login) ?
               null :
-              <HeaderNav
-                onClick={handleOnClick}
-                userLogin={authorizationStatus === AuthorizationStatus.Authorized ? userLogin : ''}
-              />
+              <nav className="header__nav">
+                <ul className="header__nav-list">
+                  {
+                    props.isAuthorized &&
+                    <li className="header__nav-item user">
+                      <div className="header__nav-profile">
+                        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                        <span className="header__user-name user__name">{props.userLogin}</span>
+                      </div>
+                    </li>
+                  }
+
+                  <li className="header__nav-item">
+                    <Link className="header__nav-link" to={href} onClick={handleOnClick}>
+                      {
+                        !props.isAuthorized ?
+                          null :
+                          <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      }
+                      <span
+                        className={signInOutClassName}
+                      >
+                        {linkText}
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
           }
         </div>
       </div>
