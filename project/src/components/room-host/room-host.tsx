87f9cb6047cb26a204e935ee00a128fc825c7f67
rrramble <rustam.arkharov@gmail.com>
2023-04-08 08:30:20 +0600

    fix: show 'Pro' badge near to Pro host avatar

--- project/src/components/room-host/room-host.tsx
+++ project/src/components/room-host/room-host.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { OfferHost } from 'src/types/types';
 
 type RoomHostProps = {
@@ -7,17 +8,23 @@ type RoomHostProps = {
 function RoomHost({ host }: RoomHostProps): JSX.Element {
   return (
     <div className="property__host-user user">
-      <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
+      <div
+        className={cn(
+          'property__avatar-wrapper user__avatar-wrapper',
+          { 'property__avatar-wrapper--pro': host.isPro },
+        )}
+      >
         <img className="property__avatar user__avatar" src={host.avatarUrl} width="74" height="74" alt="Host avatar" />
       </div>
       <span className="property__user-name">
         {host.name}
       </span>
       {
-        host.isPro &&
-        <span className="property__user-status">
-          Pro
-        </span>
+        host.isPro ?
+          <span className="property__user-status">
+            Pro
+          </span> :
+          null
       }
     </div>
   );
