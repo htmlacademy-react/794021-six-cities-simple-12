b9a2fe58b171afdeac39131782eebba0f4f944e1
rrramble <rustam.arkharov@gmail.com>
2023-04-13 14:46:13 +0600

    feat: add tests of 'data' reducer

--- project/src/store/data/data.slice.test.ts
+++ project/src/store/data/data.slice.test.ts
@@ -1,17 +1,15 @@
-import { fetchOfferAction } from '../api-actions';
+import { address } from 'faker';
 import { data, setCityNameAction } from './data.slice';
-const { reducer } = data;
+import { fetchOfferAction, fetchOffersAction, fetchReviewsAction } from 'src/store/api-actions';
+import { makeFakeOffer } from 'src/utils/mock-offer';
+import { makeMockReviewsMap } from 'src/utils/mock-review';
+import { FetchStatus } from 'src/consts/api';
 
-export enum FetchStatus { // TODO exclude importing, make local
-  FetchedWithError = 'fetched-with-error',
-  FetchedWithNoError = 'fetched-with-no-error',
-  NotStarted = 'not-started',
-  Pending = 'pending',
-}
+const { reducer } = data;
 
 describe('Reducer: data', () => {
   it('returns initial state if sent unknown action', () => {
-    expect(reducer(undefined, { type: 'FAKE_ACTION' }))
+    expect(reducer(undefined, { type: 'NON_EXISTENT_ACTION' }))
       .toEqual({
         cityName: 'Paris',
         offersFetchStatus: FetchStatus.NotStarted as FetchStatus,
@@ -23,28 +21,155 @@ describe('Reducer: data', () => {
   });
 
   it('sets city name', () => {
-    const initialState = reducer(undefined, { type: 'FAKE_ACTION' });
+    const cityName = address.cityName();
+    const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
 
     const stateToBe = {
       ...initialState,
-      cityName: 'Brazzavile',
+      cityName,
     };
 
-    expect(reducer(initialState, { type: setCityNameAction.type, payload: 'Brazzavile' }))
+    expect(reducer(initialState, { type: setCityNameAction.type, payload: cityName }))
       .toEqual(stateToBe);
   });
 
   describe('One offer fetching', () => {
+    const action = fetchOfferAction;
+
+    it('checks fulfilled status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+      const offer = makeFakeOffer();
+
+      const stateToBe = {
+        ...initialState,
+        isOfferFetching: false,
+        offers: [ offer ],
+      };
+
+      expect(reducer(initialState, { type: action.fulfilled.type, payload: offer }))
+        .toEqual(stateToBe);
+    });
+
+    it('checks adding the same offer again', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+      const offer = makeFakeOffer();
+      const stateBefore = reducer(initialState, { type: action.fulfilled.type, payload: offer });
+      const stateAfter = reducer(stateBefore, { type: action.fulfilled.type, payload: offer });
+
+      expect(stateBefore)
+        .toStrictEqual(stateAfter);
+    });
+
     it('checks pending status', () => {
-      const initialState = reducer(undefined, { type: 'FAKE_ACTION' });
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
 
       const stateToBe = {
         ...initialState,
         isOfferFetching: true,
       };
 
-      expect(reducer(initialState, { type: fetchOfferAction.pending.type }))
+      expect(reducer(initialState, { type: action.pending.type }))
         .toEqual(stateToBe);
+    });
+
+    it('checks rejected status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+
+      const stateToBe = {
+        ...initialState,
+        isOfferFetching: false,
+      };
+
+      expect(reducer(initialState, { type: action.rejected.type }))
+        .toEqual(stateToBe);
+    });
+  });
+
+
+  describe('All offers fetching', () => {
+    const action = fetchOffersAction;
+    it('checks fulfilled status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+      const offers = new Array(30).fill(makeFakeOffer());
+
+      const stateToBe = {
+        ...initialState,
+        offers,
+        offersFetchStatus: FetchStatus.FetchedWithNoError,
+      };
+
+      expect(reducer(initialState, { type: action.fulfilled.type, payload: offers }))
+        .toEqual(stateToBe);
+    });
+
+    it('checks pending status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+
+      const stateToBe = {
+        ...initialState,
+        offersFetchStatus: FetchStatus.Pending,
+      };
+
+      expect(reducer(initialState, { type: action.pending.type }))
+        .toEqual(stateToBe);
+    });
+
+    it('checks rejected status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+
+      const stateToBe = {
+        ...initialState,
+        offersFetchStatus: FetchStatus.FetchedWithError,
+      };
+
+      expect(reducer(initialState, { type: action.rejected.type }))
+        .toEqual(stateToBe);
+    });
+  });
+
+
+  describe('Reviews fetching', () => {
+    const action = fetchReviewsAction;
+
+    it('checks fulfilled status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+      const reviewsMap = makeMockReviewsMap(1, 30);
+
+      const stateToBe = {
+        ...initialState,
+        reviewsMap,
+        areReviewsFetching: false,
+      };
+
+      expect(reducer(initialState, { type: action.fulfilled.type, payload: reviewsMap }))
+        .toEqual(stateToBe);
+    });
+
+    it('checks pending status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+
+      const stateToBe = {
+        ...initialState,
+        areReviewsFetching: true,
+      };
+
+      expect(reducer(initialState, { type: action.pending.type }))
+        .toEqual(stateToBe);
+
+    });
+
+
+    it('checks rejected status', () => {
+      const initialState = reducer(undefined, { type: 'NON_EXISTENT_ACTION' });
+
+      const stateToBe = {
+        ...initialState,
+        areReviewsFetching: false,
+      };
+
+      expect(reducer(initialState, { type: action.rejected.type }))
+        .toEqual(stateToBe);
+
     });
   });
 });
