b42bb6667dcf40050d3c3a6a4e219ef17f138dff
rrramble <rustam.arkharov@gmail.com>
2023-04-14 08:20:46 +0600

    style: move 'in' of static text to one-line

--- project/src/components/no-offer-block/no-offer-block.tsx
+++ project/src/components/no-offer-block/no-offer-block.tsx
@@ -12,8 +12,8 @@ function NoOfferBlock({ cityName }: NoOfferBlockProps): JSX.Element {
           <div className="cities__status-wrapper tabs__content">
             <b className="cities__status">No places to stay available</b>
             <p className="cities__status-description">
-              We could not find any property available at the moment{' '}
-              in {cityName}
+              We could not find any property available at the moment in{' '}
+              {cityName}
             </p>
           </div>
         </section>
