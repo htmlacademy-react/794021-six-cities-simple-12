eba927053150b4e720995523207525e18c239614
rrramble <rustam.arkharov@gmail.com>
2023-04-15 21:01:37 +0600

    feat: add func 'makeMockReviews()'

--- project/src/utils/mock-review.ts
+++ project/src/utils/mock-review.ts
@@ -1,5 +1,5 @@
 import { datatype, lorem, name, internet } from 'faker';
-import { OfferId, Review, Reviewer, ReviewsMap } from 'src/types/types';
+import { OfferId, Review, Reviewer, Reviews, ReviewsMap } from 'src/types/types';
 
 const makeMockReviewer = (): Reviewer => ({
   avatarUrl: internet.url(),
@@ -16,13 +16,19 @@ export const makeMockReview = (): Review => ({
   user: makeMockReviewer(),
 });
 
+export const makeMockReviews = (
+  count: number,
+): Reviews => (
+  new Array(count)
+    .fill({})
+    .map(makeMockReview)
+);
+
 export const makeMockReviewsMap = (
   offerId: OfferId,
   count: number,
-): ReviewsMap => {
-  const reviews = new Array(count).fill(makeMockReview());
-
-  return {
-    [ offerId ]: reviews,
-  };
-};
+): ReviewsMap => (
+  {
+    [ offerId ]: makeMockReviews(count),
+  }
+);
