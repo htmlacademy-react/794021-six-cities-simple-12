57d730c69f31524540ecec206ebc93707908f4f4
rrramble <rustam.arkharov@gmail.com>
2023-04-16 10:12:26 +0600

    refactor: change <BrowserHistory> to <HistoryRouter>

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { checkIfUserAuthorizedAction } from 'src/store/api-actions';
 import { store } from 'src/store';
 import { getAuthorizationStatus, getUserAvatarUrl, getUserLogin } from 'src/store/user/user.selectors';
 import { useAppSelector } from 'src/hooks';
-import PathnameChangeEffectExecutor from
-  'src/components/pathname-change-effect-executor/pathname-change-effect-executor';
 import Main from 'src/pages/main/main';
 import Login from 'src/pages/login/login';
 import Header from 'src/components/header/header';
 import NotFound from 'src/components/not-found/not-found';
-import { scrollToTop } from 'src/utils/utils';
 import { AppRoute } from 'src/consts/consts';
 import { AuthorizationStatus } from 'src/consts/api';
 import Room from 'src/pages/room/room';
@@ -31,23 +28,20 @@ function App(): JSX.Element {
   );
 
   return (
-    <BrowserRouter>
-      <PathnameChangeEffectExecutor onPathnameChange={scrollToTop} />
-      <Routes>
-        <Route path={AppRoute.Root}>
-          <Route index element={<Main headerBlock={headerBlock} />}/>
-          <Route path={AppRoute.Login} element={<Login headerBlock={headerBlock} />} />
-          <Route path={AppRoute.Offer} element={
-            <Room
-              headerBlock={headerBlock}
-              isUserLoggedIn={authorizationStatus === AuthorizationStatus.Authorized}
-            />
-          }
+    <Routes>
+      <Route path={AppRoute.Root}>
+        <Route index element={<Main headerBlock={headerBlock} />}/>
+        <Route path={AppRoute.Login} element={<Login headerBlock={headerBlock} />} />
+        <Route path={AppRoute.Offer} element={
+          <Room
+            headerBlock={headerBlock}
+            isUserLoggedIn={authorizationStatus === AuthorizationStatus.Authorized}
           />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        }
+        />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './components/app/app';
 import { store } from 'src/store/index';
+import HistoryRouter from './components/history-router/history-router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const browserHistory = createBrowserHistory();
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer />
-      <App />
+      <HistoryRouter history={browserHistory}>
+        <ToastContainer />
+        <App />
+      </HistoryRouter>
     </Provider>
   </React.StrictMode>,
 );
