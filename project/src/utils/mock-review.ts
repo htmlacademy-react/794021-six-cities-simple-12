22c58b53cf9a4fcbd2a7fd98919fd19d31a2fb2c
rrramble <rustam.arkharov@gmail.com>
2023-04-19 21:10:25 +0600

    feat: add func 'makeMockRating()'

--- project/src/utils/mock-review.ts
+++ project/src/utils/mock-review.ts
@@ -2,6 +2,8 @@ import { DeepPartial } from '@reduxjs/toolkit';
 import { datatype, lorem, name, internet, date } from 'faker';
 import { OfferId, Review, Reviewer, Reviews, ReviewsMap } from 'src/types/types';
 
+export const makeMockRating = (): number => datatype.number({ min: 1, max: 5 });
+
 const makeMockReviewer = (): Reviewer => ({
   avatarUrl: internet.url(),
   id: datatype.number(100) + 1,
@@ -9,11 +11,11 @@ const makeMockReviewer = (): Reviewer => ({
   name: name.firstName(),
 });
 
-export const makeMockReview = (attributes: DeepPartial<Review>): Review => ({
+export const makeMockReview = (attributes?: DeepPartial<Review>): Review => ({
   comment: lorem.paragraphs(2, ' '),
   date: attributes?.date ?? date.past().toISOString(),
   id: attributes?.id ?? datatype.number(1000),
-  rating: datatype.number(50) / 10,
+  rating: makeMockRating(),
   user: makeMockReviewer(),
 });
 
