004a3f76684119fa50030b8cbd2ff38ca8bec639
rrramble <rustam.arkharov@gmail.com>
2023-04-25 13:32:34 +0600

    refactor: add percent rounding edge case

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -3,10 +3,10 @@ import { Offer, OfferId, Offers, Reviews } from 'src/types/types';
 
 export function getPercentFromRating(rating: number): string {
   const roundedPercent = Math.round(rating) * RATING_TO_PERCENT_STEP;
-  if (roundedPercent > 100) {
+  if (roundedPercent >= 100) {
     return '100%';
   }
-  if (roundedPercent < 0) {
+  if (roundedPercent <= 0) {
     return '0%';
   }
   return `${roundedPercent}%`;
