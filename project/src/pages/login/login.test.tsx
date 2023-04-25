af1ddb8ffb86018b276f50adf7373a508ac40b57
rrramble <rustam.arkharov@gmail.com>
2023-04-25 18:09:39 +0600

    test: add tests covering invalid and valid password entering

--- project/src/pages/login/login.test.tsx
+++ project/src/pages/login/login.test.tsx
@@ -1,36 +1,48 @@
-import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { Action } from '@reduxjs/toolkit';
 import { HelmetProvider } from 'react-helmet-async';
-import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
-import { address } from 'faker';
+import { address, datatype, internet, lorem } from 'faker';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { logUserInAction } from 'src/store/api-user/api-user.actions';
 import Login from './login';
 import HistoryRouter from 'src/components/history-router/history-router';
 import { AuthorizationStatus } from 'src/consts/api';
 import { AppRoute } from 'src/consts/consts';
 import { DomainNamespace } from 'src/consts/domain';
+import { createAPI } from 'src/services/api';
+import { AppState } from 'src/types/store';
+import { setUserLoginAction } from 'src/store/user/user.slice';
 
 const history = createMemoryHistory();
 
-const makeMockStore = configureMockStore();
+const api = createAPI();
+const middlewares = [ thunk ];
+const makeMockStore = configureMockStore<
+  AppState,
+  Action<string>,
+  ThunkDispatch<AppState, typeof api, Action>
+>(middlewares);
 
-const userAuthorizedState = {
+const commonMockState = {
   [ DomainNamespace.BusinessData ]: { cityName: address.cityName() },
-  [ DomainNamespace.User ]: {
-    authorizationStatus: AuthorizationStatus.NotAuthorized,
-  }
 };
 
-const userNotAuthorizedState = {
-  [ DomainNamespace.BusinessData ]: { cityName: address.cityName() },
-  [ DomainNamespace.User ]: {
-    authorizationStatus: AuthorizationStatus.Authorized,
-  }
-};
 
 describe('Component: <Login>', () => {
-  it('renders correctly', () => {
-    const mockStore = makeMockStore(userAuthorizedState);
+  it('renders correctly, user is not authorized', () => {
+    const mockState = {
+      ...commonMockState,
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.NotAuthorized,
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+    history.push(AppRoute.Login);
 
     render(
       <Provider store={mockStore}>
@@ -41,6 +53,9 @@ describe('Component: <Login>', () => {
         </HistoryRouter>
       </Provider>
     );
+
+    expect(history.location.pathname)
+      .toEqual(AppRoute.Login);
 
     expect(screen.getByRole('heading'))
       .toHaveAccessibleName(/Sign in/i);
@@ -54,7 +69,14 @@ describe('Component: <Login>', () => {
 
 
   it('redirects to <Main> page if user is authorized', () => {
-    const mockStore = makeMockStore(userNotAuthorizedState);
+    const mockState = {
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.Authorized,
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+    history.push(AppRoute.Login);
 
     render(
       <Provider store={mockStore}>
@@ -68,5 +90,143 @@ describe('Component: <Login>', () => {
 
     expect(history.location.pathname)
       .toEqual(AppRoute.Root);
+  });
+
+
+  it('goes to <Main> page if clicked on the city link', () => {
+    const mockState = {
+      ...commonMockState,
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.NotAuthorized,
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+    history.push(AppRoute.Login);
+
+    render(
+      <Provider store={mockStore}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <Login />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const cityLink = screen.getByTestId('login__city-link');
+    fireEvent(cityLink, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    expect(history.location.pathname)
+      .toEqual(AppRoute.Root);
+  });
+});
+
+
+describe('Component: <Login>. Set login/password', () => {
+  it('sets login (email)', async () => {
+    const mockState = {
+      ...commonMockState,
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.NotAuthorized,
+        login: '',
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+
+    render(
+      <Provider store={mockStore}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <Login />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const loginCharactersCount = datatype.number(10);
+    const mockLogin = lorem.word(loginCharactersCount);
+    const loginInput = screen.getByTestId('login__email-input');
+
+    await userEvent.type(loginInput, mockLogin);
+
+    const actionNames = mockStore.getActions().map(({ type }) => type);
+
+    expect(actionNames.length)
+      .toEqual(loginCharactersCount);
+
+    actionNames.forEach((actionName) => {
+      expect(actionName)
+        .toEqual(setUserLoginAction.type);
+    });
+  });
+
+
+  it('submits when password is not valid', async () => {
+    const mockState = {
+      ...commonMockState,
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.NotAuthorized,
+        login: internet.email(),
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+
+    render(
+      <Provider store={mockStore}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <Login />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const password = lorem.word(1);
+    const passwordInput = screen.getByTestId('login__password-input');
+    const submitButton = screen.getByTestId('login__submit-button');
+
+    await userEvent.type(passwordInput, password || '{tab}');
+    fireEvent(submitButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    const actionNames = mockStore.getActions().map(({ type }) => type);
+    expect(actionNames)
+      .toEqual([]);
+  });
+
+
+  it('submits when password is valid', async () => {
+    const mockState = {
+      ...commonMockState,
+      [ DomainNamespace.User ]: {
+        authorizationStatus: AuthorizationStatus.NotAuthorized,
+        login: internet.email(),
+      },
+    };
+
+    const mockStore = makeMockStore(mockState);
+
+    render(
+      <Provider store={mockStore}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <Login />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const password = `${lorem.word(1)}${datatype.number()}`;
+    const passwordInput = screen.getByTestId('login__password-input');
+    const submitButton = screen.getByTestId('login__submit-button');
+
+    await userEvent.type(passwordInput, password);
+    fireEvent(submitButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    const actionNames = mockStore.getActions().map(({ type }) => type);
+    expect(actionNames)
+      .toEqual([ logUserInAction.pending.type ]);
   });
 });
