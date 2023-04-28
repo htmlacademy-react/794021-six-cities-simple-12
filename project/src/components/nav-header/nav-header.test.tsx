e6efbec12905e2e66aef54e4207542dd5f4dd480
rrramble <rustam.arkharov@gmail.com>
2023-04-28 09:38:13 +0600

    test: add some of <NavHeader> component

--- project/src/components/nav-header/nav-header.test.tsx
+++ project/src/components/nav-header/nav-header.test.tsx
@@ -1,5 +1,5 @@
 import thunk, { ThunkDispatch } from 'redux-thunk';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Action } from '@reduxjs/toolkit';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { internet } from 'faker';
@@ -10,6 +10,7 @@ import { AppState } from 'src/types/store';
 import { Provider } from 'react-redux';
 import { AuthorizationStatus } from 'src/consts/api';
 import { DomainNamespace } from 'src/consts/domain';
+import { logUserOutAction } from 'src/store/api-user/api-user.actions';
 
 const api = createAPI();
 const middlewares = [ thunk ];
@@ -93,5 +94,23 @@ describe('Component: <Header>', () => {
     expect(screen.queryByText(mockLogin)).not.toBeInTheDocument();
     expect(screen.getByText('Sign in')).toBeInTheDocument();
     expect(screen.queryByText('Sign out')).not.toBeInTheDocument();
+  });
+
+
+  it('dispatches "sign out" when clicked', () => {
+    const mockStore = makeMockStore(userAuthrizedState);
+
+    render(
+      <Provider store={mockStore}>
+        <MockBrowserRouterWrapper>
+          <NavHeader />
+        </MockBrowserRouterWrapper>
+      </Provider>
+    );
+
+    fireEvent.click(screen.getByRole('link', { name: 'Sign out' }));
+
+    expect(mockStore.getActions().map((action) => action.type))
+      .toEqual([ logUserOutAction.pending.type ]);
   });
 });
